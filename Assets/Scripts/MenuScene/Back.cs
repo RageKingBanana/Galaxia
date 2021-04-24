9920d3d8186e3ed5084d276bfde060e4bc964e17
Ryan Verzo <ryanemmanuelvrz345@gmail.com>
2021-04-24 19:47:29 +0800

    commit

--- Assets/Scripts/MenuScene/Back.cs
+++ Assets/Scripts/MenuScene/Back.cs
@@ -8,6 +8,6 @@ public class Back : MonoBehaviour
     // Start is called before the first frame update
     public void MainMenu()
     {
-        SceneManager.LoadScene ("MAINMENU");
+        SceneManager.LoadSceneAsync(0);
     }
 }
